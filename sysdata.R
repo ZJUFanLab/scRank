b588aedc23fd67d85faa4cc85613ccb246e3efb7
Cheng yu, Li <53106913+Lee0498@users.noreply.github.com>
2022-12-05 03:47:29 +0800

    Add files via upload

---
+++ sysdata.R
@@ -0,0 +1,7 @@
+#' utile_database
+#' 
+#' 1. database containing the drug information of inhibitor with drug name, drug target, and interaction type sourced from DGIdb updated on December 30, 2021.
+#' 2. database containing the gene information of \code{human} and \code{mouse} updated on June 30, 2021.
+#' 3. database containing the transcription factors information of \code{human} and \code{mouse}.
+#' @source \url{dgidb.org/drugs} \url{https://www.ncbi.nlm.nih.gov/gene} \url{http://bioinfo.life.hust.edu.cn/AnimalTFDB/#!/}
+"utile_database" 
